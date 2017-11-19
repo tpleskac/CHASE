a2eb4223912a0e6d932927faf094d378d85bdbf2
tpleskac <tim.pleskac@gmail.com>
2017-11-19 17:35:08 +0100

    Fixed the weighting func. to order outcomes

--- prelec.m
+++ prelec.m
@@ -1,3 +1,50 @@
-function w = prelec(p,gamma,delta)
+function [wout,xout] = prelec(p,x,gamma,delta)
+% first identify if gains or losses
+gainTest = x>0;
+lossTest = x<0;
+zeroTest = x==0;
 
-w = exp(-delta.*(-log(p)).^gamma ); 
+if(sum(gainTest)>0 && sum(lossTest)>0) %mixed
+    %gains
+    theGains = x(x>0);
+    probGains = p(x>0);
+    [gainSort, isort] = sort(theGains); 
+    psort = probGains(isort);
+    wGains = weightCalc(psort,gamma,delta); 
+   
+    %losses
+    theLosses = x(x<=0);
+    probLosses = p(x<=0);
+    [lossSort, isort] = sort(theLosses,'descend'); 
+    psort = probLosses(isort);
+    wLosses = weightCalc(psort,gamma,delta); 
+    
+    wout = [wLosses wGains];
+    xout = [lossSort gainSort];
+
+else
+    
+    if(sum(gainTest)>0 && sum(lossTest)==0)%gains
+        [xsort, isort] = sort(x); 
+        psort = p(isort);
+    else%(sum(gainTest)==0 && sum(lossTest)>0)%losses
+        [xsort, isort] = sort(x,'descend'); 
+        psort = p(isort); 
+    end
+    w = weightCalc(psort,gamma,delta);
+    wout = w;
+    xout = xsort;
+    %w(:,2) = wr(:,2)-wr(:,1);
+    %w(:,1) = wr(:,1);
+    
+end
+
+function wcalc = weightCalc(psort,gamma,delta)
+
+r = cumsum(psort,2);
+wr = exp(-delta.*(-log(r)).^gamma ); 
+wr(r==0)= 0;
+wr(r==1) =1; 
+wrTemp = zeros(1,length(wr));
+wrTemp(:) = [0 wr(1:(length(wr)-1))]; 
+wcalc = wr-wrTemp;
---
+++ prospect.m
@@ -0,0 +1,46 @@
+function logLike = prospect(xin, tbl, bounder, bounds)
+%power_mle the log-likelihood function of the power model
+if (bounder)
+   gamma = parameter_bounder(xin(1), 1, bounds.gamma); 
+   beta = parameter_bounder(xin(2), 1, bounds.theta); 
+else
+    gamma = xin(1);
+    beta = xin(2); 
+end
+
+alpha = 1;%utility for gains
+beta = 1; %utility for losses
+lambda =1;%loss aversion
+delta = 1;%height for weight function
+
+wLow = zeros(height(tbl),2);
+wHigh = zeros(height(tbl),2);
+xLow = zeros(height(tbl),2);
+xHigh = zeros(height(tbl),2);
+
+for i = 1:height(tbl)
+    [wLow(i,:), xLow(i,:)] = prelec([tbl.Lp0(i) tbl.Lp1(i)],[tbl.Lx0(i) tbl.Lx1(i)],gamma,delta);
+    [wHigh(i,:), xHigh(i,:)]  = prelec([tbl.Hp0(i) tbl.Hp1(i)],[tbl.Hx0(i) tbl.Hx1(i)],gamma,delta);
+end
+
+uLow = zeros(height(tbl),2);
+uHigh = zeros(height(tbl),2);
+
+uLow= utility( xLow, alpha,beta,lambda  );
+uHigh = utility( xHigh, alpha,beta,lambda );
+
+%lowOutcomes = [tbl.Lx0 tbl.Lx1];
+%highOutcomes = [tbl.Hx0 tbl.Hx1];
+
+%lowProb = [tbl.Lp0 tbl.Lp1];
+%highProb = [tbl.Hp0 tbl.Hp1];
+
+%wLow = prelec(lowProb,gamma,delta);
+%wHigh = prelec(highProb,gamma,delta);
+
+prospectLow = sum(wLow.*uLow,2);
+prospectHigh = sum(wHigh.*uHigh,2);
+
+probChooseH = 1./(1+exp( -beta.*( prospectHigh - prospectLow ) ) ); 
+
+logLike = -1.*sum(tbl.choice.*log(probChooseH) + (1-tbl.choice).*log(1-probChooseH)); 
